57d9a101fe5ea36e875353b5b3f4b00dd75266b6
Jeongwon Park <sanguineman@naver.com>
2026-06-24 16:01:47 +0900

    fix(mutate): nat and int to use constants from an input program

--- p4spec/lib/backend-testgen-neg/mutate.ml
+++ p4spec/lib/backend-testgen-neg/mutate.ml
@@ -31,34 +31,155 @@ let wrap_value (typ : typ') (value : value') : value =
 let wrap_value_opt (typ : typ') (value_opt : value' option) : value option =
   Option.map (wrap_value typ) value_opt
 
+let mixop_starts_with_atom (atom_target : atom') (mixop : mixop) : bool =
+  match mixop with
+  | ({ it = atom; _ } :: _) :: _ -> Atom.eq atom atom_target
+  | _ -> false
+
+type int_bounds = { min_value : Bigint.t; max_value : Bigint.t }
+
+type num_context = {
+  nat_bounds : int_bounds option;
+  int_bounds : int_bounds option;
+}
+
+let one = Bigint.of_int 1
+
+let two_to_32 = Bigint.of_string "4294967296"
+
+let dedup_bigints (values : Bigint.t list) : Bigint.t list =
+  List.fold_left
+    (fun values_deduped value ->
+      if
+        List.exists
+          (fun value_deduped -> Bigint.compare value value_deduped = 0)
+          values_deduped
+      then values_deduped
+      else value :: values_deduped)
+    [] values
+  |> List.rev
+
+let max_bigint (a : Bigint.t) (b : Bigint.t) : Bigint.t =
+  if Bigint.compare a b < 0 then b else a
+
+let default_nat_values : Bigint.t list =
+  [ one; Bigint.of_int 32; Bigint.of_int 65536; two_to_32 ]
+
+let default_int_values : Bigint.t list =
+  [ Bigint.of_int (-16); Bigint.of_int 0; Bigint.of_int 65536; two_to_32 ]
+
+let input_nat_values_of_bounds (bounds : int_bounds option) : Bigint.t list =
+  match bounds with
+  | None -> []
+  | Some { min_value; max_value } ->
+      [
+        one;
+        Bigint.(min_value - one) |> max_bigint one;
+        Bigint.(max_value + one);
+        two_to_32;
+      ]
+      |> dedup_bigints
+
+let input_int_values_of_bounds (bounds : int_bounds option) : Bigint.t list =
+  match bounds with
+  | None -> []
+  | Some { min_value; max_value } ->
+      [
+        Bigint.(min_value - of_int 1);
+        Bigint.of_int 0;
+        Bigint.(max_value + of_int 1);
+        two_to_32;
+      ]
+      |> dedup_bigints
+
+let random_select_from_num_pools (values_default : Bigint.t list)
+    (values_input : Bigint.t list) : Bigint.t option =
+  let pools =
+    match values_input with
+    | [] -> [ values_default ]
+    | _ :: _ -> [ values_default; values_input ]
+  in
+  let* values = Rand.random_select pools in
+  Rand.random_select values
+
+let update_int_bounds (bounds : int_bounds option) (value : Bigint.t) :
+    int_bounds option =
+  let update_min current =
+    if Bigint.compare value current < 0 then value else current
+  in
+  let update_max current =
+    if Bigint.compare value current > 0 then value else current
+  in
+  match bounds with
+  | None -> Some { min_value = value; max_value = value }
+  | Some { min_value; max_value } ->
+      Some
+        { min_value = update_min min_value; max_value = update_max max_value }
+
+let collect_texts (vdg : Dep.Graph.t) : value' list =
+  List.init (vdg.root + 1) Fun.id
+  |> List.filter_map (fun vid ->
+         let* mirror, _ = Dep.Graph.find_node vdg vid in
+         match mirror.it with TextN text -> Some (TextV text) | _ -> None)
+
+let collect_int_bounds (vdg : Dep.Graph.t) : int_bounds option =
+  List.init (vdg.root + 1) Fun.id
+  |> List.fold_left
+       (fun bounds vid ->
+         match Dep.Graph.find_node vdg vid with
+         | Some (mirror, taint) when Dep.Node.is_source taint -> (
+             match mirror.it with
+             | NumN num -> Xl.Num.to_int num |> update_int_bounds bounds
+             | _ -> bounds)
+         | _ -> bounds)
+       None
+
+let collect_nat_bounds (vdg : Dep.Graph.t) : int_bounds option =
+  List.init (vdg.root + 1) Fun.id
+  |> List.fold_left
+       (fun bounds vid ->
+         match Dep.Graph.find_node vdg vid with
+         | Some (mirror, taint) when Dep.Node.is_source taint -> (
+             match mirror.it with
+             | NumN num ->
+                 let value = Xl.Num.to_int num in
+                 if Bigint.compare value one >= 0 then
+                   update_int_bounds bounds value
+                 else bounds
+             | _ -> bounds)
+         | _ -> bounds)
+       None
+
+let collect_num_context (vdg : Dep.Graph.t) : num_context =
+  {
+    nat_bounds = collect_nat_bounds vdg;
+    int_bounds = collect_int_bounds vdg;
+  }
+
 (* Type-driven mutation *)
 
 let rec gen_from_typ (depth : int) (tdenv : TDEnv.t) (texts : value' list)
-    (typ : typ) : value option =
-  if depth <= 0 then None else gen_from_typ' depth tdenv texts typ
+    (nums : num_context) (typ : typ) : value option =
+  if depth <= 0 then None else gen_from_typ' depth tdenv texts nums typ
 
 and gen_from_typ' (depth : int) (tdenv : TDEnv.t) (texts : value' list)
-    (typ : typ) : value option =
+    (nums : num_context) (typ : typ) : value option =
   let depth = depth - 1 in
   match typ.it with
   | BoolT ->
       [ BoolV true; BoolV false ] |> Rand.random_select |> wrap_value_opt typ.it
   | NumT `NatT ->
-      [
-        NumV (`Nat (Bigint.of_int 1));
-        NumV (`Nat (Bigint.of_int 32));
-        NumV (`Nat (Bigint.of_int 65536));
-        NumV (`Nat (Bigint.of_int 4294967296));
-      ]
-      |> Rand.random_select |> wrap_value_opt typ.it
+      let* value =
+        input_nat_values_of_bounds nums.nat_bounds
+        |> random_select_from_num_pools default_nat_values
+      in
+      NumV (`Nat value) |> Option.some |> wrap_value_opt typ.it
   | NumT `IntT ->
-      [
-        NumV (`Int (Bigint.of_int (-16)));
-        NumV (`Int (Bigint.of_int 0));
-        NumV (`Int (Bigint.of_int 65536));
-        NumV (`Int (Bigint.of_int 4294967296));
-      ]
-      |> Rand.random_select |> wrap_value_opt typ.it
+      let* value =
+        input_int_values_of_bounds nums.int_bounds
+        |> random_select_from_num_pools default_int_values
+      in
+      NumV (`Int value) |> Option.some |> wrap_value_opt typ.it
   | TextT -> texts |> Rand.random_select |> wrap_value_opt typ.it
   | VarT (tid, targs) -> (
       let td = TDEnv.find_opt tid tdenv in
@@ -68,13 +189,13 @@ and gen_from_typ' (depth : int) (tdenv : TDEnv.t) (texts : value' list)
           match td.it with
           | PlainT typ ->
               typ |> Type.Subst.subst_typ theta
-              |> gen_from_typ depth tdenv texts
+              |> gen_from_typ depth tdenv texts nums
           | StructT typfields ->
               let atoms, typs = List.split typfields in
               let* values =
                 typs
                 |> Type.Subst.subst_typs theta
-                |> gen_from_typs depth tdenv texts
+                |> gen_from_typs depth tdenv texts nums
               in
               let valuefields = List.combine atoms values in
               StructV valuefields |> Option.some |> wrap_value_opt typ.it
@@ -84,9 +205,17 @@ and gen_from_typ' (depth : int) (tdenv : TDEnv.t) (texts : value' list)
                 |> List.map (fun (nottyp, _, _) ->
                        Mixfix.map (Type.Subst.subst_typ theta) nottyp.it)
               in
+              let nottyps' =
+                if String.equal tid.it "vibinop" then
+                  List.filter
+                    (fun (mixop, _) ->
+                      mixop_starts_with_atom (Atom "Shuffle") mixop)
+                    nottyps'
+                else nottyps'
+              in
               let expand_nottyp' nottyp' =
                 let mixop, typs = Mixfix.split nottyp' in
-                let* values = gen_from_typs depth tdenv texts typs in
+                let* values = gen_from_typs depth tdenv texts nums typs in
                 CaseV (Mixfix.fill mixop values) |> Option.some
               in
               List.map expand_nottyp' nottyps'
@@ -94,7 +223,7 @@ and gen_from_typ' (depth : int) (tdenv : TDEnv.t) (texts : value' list)
               |> Rand.random_select |> wrap_value_opt typ.it)
       | _ -> None)
   | TupleT typs_inner ->
-      let* values_inner = gen_from_typs depth tdenv texts typs_inner in
+      let* values_inner = gen_from_typs depth tdenv texts nums typs_inner in
       TupleV values_inner |> Option.some |> wrap_value_opt typ.it
   | IterT (_, Opt) when depth = 0 ->
       OptV None |> Option.some |> wrap_value_opt typ.it
@@ -102,7 +231,9 @@ and gen_from_typ' (depth : int) (tdenv : TDEnv.t) (texts : value' list)
       let choices : value' option list =
         [
           OptV None |> Option.some;
-          (let* value_inner = gen_from_typ depth tdenv texts typ_inner in
+          (let* value_inner =
+             gen_from_typ depth tdenv texts nums typ_inner
+           in
            OptV (Some value_inner) |> Option.some);
         ]
       in
@@ -113,29 +244,30 @@ and gen_from_typ' (depth : int) (tdenv : TDEnv.t) (texts : value' list)
   | IterT (_, List) when depth = 0 ->
       ListV [] |> Option.some |> wrap_value_opt typ.it
   | IterT (typ_inner, List) ->
-      let len = Random.int 3 in
+      let* len = Rand.random_select [ 2; 4; 8; 16 ] in
       let* values_inner =
-        List.init len (fun _ -> typ_inner) |> gen_from_typs depth tdenv texts
+        List.init len (fun _ -> typ_inner)
+        |> gen_from_typs depth tdenv texts nums
       in
       ListV values_inner |> Option.some |> wrap_value_opt typ.it
   | FuncT _ -> None
 
 and gen_from_typs (depth : int) (tdenv : TDEnv.t) (texts : value' list)
-    (typs : typ list) : value list option =
+    (nums : num_context) (typs : typ list) : value list option =
   if depth <= 0 then None
   else
     List.fold_left
       (fun values_opt typ ->
         let* values = values_opt in
-        let* value = gen_from_typ depth tdenv texts typ in
+        let* value = gen_from_typ depth tdenv texts nums typ in
         Some (values @ [ value ]))
       (Some []) typs
 
-let mutate_type_driven (tdenv : TDEnv.t) (texts : value' list) (value : value) :
-    (kind * value) option =
+let mutate_type_driven (tdenv : TDEnv.t) (texts : value' list)
+    (nums : num_context) (value : value) : (kind * value) option =
   let typ = value.note.typ $ no_region in
   let depth = Random.int 4 + 1 in
-  let value_opt = gen_from_typ depth tdenv texts typ in
+  let value_opt = gen_from_typ depth tdenv texts nums typ in
   Option.map (fun value -> (GenFromTyp, value)) value_opt
 
 (* Constructor mutation *)
@@ -274,13 +406,13 @@ let mutate_list (value : value) : (kind * value) option =
   mutation ()
 
 let mutate_node (tdenv : TDEnv.t) (mixopenv : MixopEnv.t) (texts : value' list)
-    (value : value) : (kind * value) option =
+    (nums : num_context) (value : value) : (kind * value) option =
   match value.it with
   | ListV _ ->
       let* mutation =
         [
           (fun () -> mutate_list value);
-          (fun () -> mutate_type_driven tdenv texts value);
+          (fun () -> mutate_type_driven tdenv texts nums value);
         ]
         |> Rand.random_select
       in
@@ -289,15 +421,15 @@ let mutate_node (tdenv : TDEnv.t) (mixopenv : MixopEnv.t) (texts : value' list)
       let* mutation =
         [
           (fun () -> mutate_mixop mixopenv value);
-          (fun () -> mutate_type_driven tdenv texts value);
+          (fun () -> mutate_type_driven tdenv texts nums value);
         ]
         |> Rand.random_select
       in
       mutation ()
-  | _ -> mutate_type_driven tdenv texts value
+  | _ -> mutate_type_driven tdenv texts nums value
 
 let mutate_walk (tdenv : TDEnv.t) (mixopenv : MixopEnv.t) (texts : value' list)
-    (value : value) : (kind * value) option =
+    (nums : num_context) (value : value) : (kind * value) option =
   (* Compute the best path to a leaf node in the value subtree *)
   let key_max = ref min_float in
   let path_best = ref [] in
@@ -332,7 +464,7 @@ let mutate_walk (tdenv : TDEnv.t) (mixopenv : MixopEnv.t) (texts : value' list)
     let typ = value.note.typ in
     match (path, value) with
     | [], value ->
-        let* kind, value = mutate_node tdenv mixopenv texts value in
+        let* kind, value = mutate_node tdenv mixopenv texts nums value in
         kind_found := kind |> Option.some;
         value |> Option.some
     | idx :: path, value -> (
@@ -389,7 +521,8 @@ let find_parent (vdg : Dep.Graph.t) (vid_source : vid) : vid option =
 (* Entry point for mutation *)
 
 let mutate (tdenv : TDEnv.t) (mixopenv : MixopEnv.t) (texts : value' list)
-    (vdg : Dep.Graph.t) (vid_source : vid) : (kind * value * value) option =
+    (nums : num_context) (vdg : Dep.Graph.t) (vid_source : vid) :
+    (kind * value * value) option =
   (* Expand the node randomly *)
   let expansions =
     [
@@ -406,19 +539,18 @@ let mutate (tdenv : TDEnv.t) (mixopenv : MixopEnv.t) (texts : value' list)
     Dep.Graph.reassemble_graph vdg VIdMap.empty vid_to_mutate
   in
   (* Mutate the node *)
-  let* kind, value_mutated = mutate_walk tdenv mixopenv texts value_to_mutate in
+  let* kind, value_mutated =
+    mutate_walk tdenv mixopenv texts nums value_to_mutate
+  in
   (kind, value_to_mutate, value_mutated) |> Option.some
 
 let mutates (fuel_mutate : int) (tdenv : TDEnv.t) (mixopenv : MixopEnv.t)
     (vdg : Dep.Graph.t) (vid_source : vid) : (kind * value * value) list =
   (* Collect the text pool *)
-  let texts =
-    List.init (vdg.root + 1) Fun.id
-    |> List.filter_map (fun vid ->
-           let* mirror, _ = Dep.Graph.find_node vdg vid in
-           match mirror.it with TextN text -> Some (TextV text) | _ -> None)
-  in
+  let texts = collect_texts vdg in
   let texts = texts @ [ TextV "lazy"; TextV "fox" ] in
+  let nums = collect_num_context vdg in
   (* Do mutations *)
-  List.init fuel_mutate (fun _ -> mutate tdenv mixopenv texts vdg vid_source)
+  List.init fuel_mutate (fun _ ->
+      mutate tdenv mixopenv texts nums vdg vid_source)
   |> List.filter_map Fun.id
